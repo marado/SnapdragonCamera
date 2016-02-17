f876a8f2d53bd3a00788c9f260aee801fa8938f9
Jay Wang <jaywang@codeaurora.org>
2016-02-17 11:59:42 -0800

    SnapdragonCamera: Add persist property to skip the memory check
    Adding a property to skeip the memory check during continuous shot
    
    CRs-Fixed: 978195
    Change-Id: I45b24edd528339e9ca716581381c6890f355c8b7

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -206,10 +206,11 @@ public class PhotoModule
 
     private boolean mFaceDetectionStarted = false;
 
-    private static final String PERSIST_LONG_ENABLE = "persist.camera.longshot.enable";
     private static final String PERSIST_LONG_SAVE = "persist.camera.longshot.save";
     private static final String PERSIST_PREVIEW_RESTART = "persist.camera.feature.restart";
     private static final String PERSIST_CAPTURE_ANIMATION = "persist.camera.capture.animate";
+    private static final boolean PERSIST_SKIP_MEM_CHECK =
+            android.os.SystemProperties.getBoolean("persist.camera.perf.skip_memck", false);
 
     private static final int MINIMUM_BRIGHTNESS = 0;
     private static final int MAXIMUM_BRIGHTNESS = 6;
@@ -912,6 +913,10 @@ public class PhotoModule
 
     // TODO: need to check cached background apps memory and longshot ION memory
     private boolean isLongshotNeedCancel() {
+        if (PERSIST_SKIP_MEM_CHECK == true) {
+            return false;
+        }
+
         if (SECONDARY_SERVER_MEM == 0) {
             ActivityManager am = (ActivityManager) mActivity.getSystemService(
                     Context.ACTIVITY_SERVICE);
