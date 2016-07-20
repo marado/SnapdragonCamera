8323e5ef54f293d1d79f37b951dd335469ab3423
Jay Wang <jaywang@codeaurora.org>
2016-07-19 18:11:05 -0700

    SnapdragonCamera: Remove custom location permission request window
    When user enables the GPS locaion setting, use Android's run-time
    permission request interface to get the permission.
    
    Change-Id: Ib17171aeff5ccf20e2b00da08243dee1a3383611
    CRs-Fixed: 1038039

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -1453,6 +1453,10 @@ public class PhotoMenu extends MenuController
         }
         updateFilterModeIcon(pref, pref);
 
+        if (same(pref, CameraSettings.KEY_RECORD_LOCATION, "on")) {
+            mActivity.requestLocationPermission();
+        }
+
         super.onSettingChanged(pref);
     }
 
