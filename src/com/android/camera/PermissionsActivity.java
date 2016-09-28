7b7d0f958e51068e0d8fa3626d5f65d20e514c13
Suman Mukherjee <sumam@codeaurora.org>
2016-09-28 15:04:16 +0530

    SnapdragonCamera:Fix Camera GTS failture issue.
    GTS test will fail if use permission activity to return
    the result, so set FLAG_ACTIVITY_FORWARD_RESULT flag for
    the intent and let CameraActivity return the result directly.
    And add URI read permission to the return intent
    
    Change-Id: I224789bbd430d977234d2bcbea4e38e957b81142
    CRs-Fixed: 1059964

--- src/com/android/camera/PermissionsActivity.java
+++ src/com/android/camera/PermissionsActivity.java
@@ -182,14 +182,16 @@ public class PermissionsActivity extends Activity {
 
     private void handlePermissionsSuccess() {
         if (mIntent != null) {
-           mIsReturnResult = true;
-           mIntent.setClass(this, CameraActivity.class);
-           startActivityForResult(mIntent, 1);
+            mIsReturnResult = true;
+            mIntent.setClass(this, CameraActivity.class);
+            mIntent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+            startActivity(mIntent);
+            finish();
         } else {
-           mIsReturnResult = false;
-           Intent intent = new Intent(this, CameraActivity.class);
-           startActivity(intent);
-           finish();
+            mIsReturnResult = false;
+            Intent intent = new Intent(this, CameraActivity.class);
+            startActivity(intent);
+            finish();
         }
     }
 
