69510e0c50e870bf4094d0f089275473f3b4dab0
xianming wang <mingwax@codeaurora.org>
2018-11-26 17:20:16 +0800

    SnapdragonCamera: Fix NP when mPreviewCaptureResult is NULL
    Fix NP when mPreviewCaptureResult is NULL in first enter camera.
    
    Change-Id: I29e6a6d93f06265e4332d343a9c8240eff8d34e8
    Signed-off-by: Ashok Kundurthi <askund@codeaurora.org>

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -1810,9 +1810,10 @@ public class CaptureModule implements CameraModule, PhotoController,
     private void takePicture() {
         Log.d(TAG, "takePicture");
         mUI.enableShutter(false);
-        if (mSettingsManager.isZSLInHALEnabled()&&
+        if (mSettingsManager.isZSLInHALEnabled() &&
+                !isFlashOn(getMainCameraId()) && (mPreviewCaptureResult != null &&
                 mPreviewCaptureResult.get(CaptureResult.CONTROL_AE_STATE) !=
-                        CameraMetadata.CONTROL_AE_STATE_FLASH_REQUIRED) {
+                        CameraMetadata.CONTROL_AE_STATE_FLASH_REQUIRED)) {
             takeZSLPictureInHAL();
         } else {
             if (isBackCamera()) {
